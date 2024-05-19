9086000a03a529154fbfe2045978d7b4af306e11
weerachit utnok <weerachit.utnok01@gmail.com>
2024-05-19 19:38:45 +0700

    fix: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,6 @@
 
 import { Avatar, Image, NextUIProvider } from '@nextui-org/react';
 
-import { Lilita_One } from 'next/font/google'
-
-const lilita_One = Lilita_One({
-  weight: '400',
-  subsets: ['latin'],
-})
-
-
 const data_mockup = [
   {
     title: "- Ohi Appication UX/UI 🌼",
@@ -63,26 +55,6 @@ const data_mockup = [
 ]
 
 
-const BannerWithAvatar = () => {
-  return (
-    <div className="relative w-full h-[200px] mb-5">
-      <Image
-        src="https://i.pinimg.com/originals/02/87/d3/0287d3ba8b3330fca99f69e2001d3168.gif"
-        alt="banner"
-        className="w-[2000px] h-[200px] object-fill"
-      />
-      <div className="absolute top-8 left-1/3 md:top-10 md:left-10 z-20">
-        <Avatar
-          isBordered
-          radius="md"
-          color="secondary"
-          src="https://firebasestorage.googleapis.com/v0/b/image-profile-d0415.appspot.com/o/Work%2F373603558_3271084813183244_1796661666035531303_n.jpg?alt=media&token=5b0bd825-b8b8-4d81-948d-3f6c2ee365eb"
-          className="w-[120px] h-[120px]"
-        />
-      </div>
-    </div>
-  );
-};
 
 export default function Home() {
   return (
@@ -100,39 +72,32 @@ export default function Home() {
             </a>
           </div>
           <div className="flex space-x-0">
-            {/* {Object.entries(navItems).map(([path, { name }]) => {
-              return <NavItem key={path} path={path} name={name} />;
-            })} */}
             <a className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity p-1" aria-label="Github" role="link" href="https://github.com/HakimIno" rel="noopener noreferrer" target="_blank"><svg height="24" viewBox="0 0 24 24" width="24" className="text-default-600 dark:text-default-500"><path clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z" fill="currentColor" fill-rule="evenodd"></path></svg></a>
             <a className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-medium text-primary no-underline hover:opacity-80 active:opacity-disabled transition-opacity p-1" aria-label="Github" role="link" href="https://www.linkedin.com/in/weerachit-utnok-44b69922b/?fbclid=IwZXh0bgNhZW0CMTAAAR2XIImhR_pWvNlNlT_AlnUD1wpR9KPN_hY7kI-uxjhNmQjVuOMHkdrocSA_aem_AUNwbLTChvapFzEW8AWum-g22hhCaQKgCVY37GXaxVOXGALs0bZE7mMg1ajMgPOl5IMzQHJMXS9AV7P7YdAJfpCJ" rel="noopener noreferrer" target="_blank"><svg fill="#6b7280" height="20px" width="20px" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 310 310" stroke="#cfcfcf"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_801_"> <path id="XMLID_802_" d="M72.16,99.73H9.927c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5H72.16c2.762,0,5-2.238,5-5V104.73 C77.16,101.969,74.922,99.73,72.16,99.73z"></path> <path id="XMLID_803_" d="M41.066,0.341C18.422,0.341,0,18.743,0,41.362C0,63.991,18.422,82.4,41.066,82.4 c22.626,0,41.033-18.41,41.033-41.038C82.1,18.743,63.692,0.341,41.066,0.341z"></path> <path id="XMLID_804_" d="M230.454,94.761c-24.995,0-43.472,10.745-54.679,22.954V104.73c0-2.761-2.238-5-5-5h-59.599 c-2.762,0-5,2.239-5,5v199.928c0,2.762,2.238,5,5,5h62.097c2.762,0,5-2.238,5-5v-98.918c0-33.333,9.054-46.319,32.29-46.319 c25.306,0,27.317,20.818,27.317,48.034v97.204c0,2.762,2.238,5,5,5H305c2.762,0,5-2.238,5-5V194.995 C310,145.43,300.549,94.761,230.454,94.761z"></path> </g> </g></svg></a>
           </div>
         </nav>
 
-
         <div className="items-start max-w-5xl w-full py-10">
-          {/*   eslint-disable-next-line react/no-unescaped-entities */}
-
-
-          {BannerWithAvatar()}
-
+          <div className="relative w-full h-[200px] mb-5">
+            <Image
+              src="https://i.pinimg.com/originals/02/87/d3/0287d3ba8b3330fca99f69e2001d3168.gif"
+              alt="banner"
+              className="w-[2000px] h-[200px] object-fill"
+            />
+            <div className="absolute top-8 left-1/3 md:top-10 md:left-10 z-20">
+              <Avatar
+                isBordered
+                radius="md"
+                color="secondary"
+                src="https://firebasestorage.googleapis.com/v0/b/image-profile-d0415.appspot.com/o/Work%2F373603558_3271084813183244_1796661666035531303_n.jpg?alt=media&token=5b0bd825-b8b8-4d81-948d-3f6c2ee365eb"
+                className="w-[120px] h-[120px]"
+              />
+            </div>
+          </div>
           <h3 className='font-mono font-bold text-lg my-3'>hey, I'm weerachit 👋</h3>
-
-
           <p className='font-mono text-sm '>
-            {/*   eslint-disable-next-line react/no-unescaped-entities */}
             I'm a frontend developer,  a passionate and results-driven full-stack developer with expertise in front-end technologies such as React, React Native, Next.js, TypeScript, and JavaScript. On the back-end, I specialize in Node.js and Go, ensuring a seamless integration of the entire development stack.
           </p>
-
-
-
-          {/* <Card className="col-span-12 sm:col-span-4 h-[350px] w-full my-6">
-              <Image
-                removeWrapper
-                alt="Card background"
-                className="z-0 w-full h-full rounded-xl"
-                src="https://media2.giphy.com/media/2IudUHdI075HL02Pkk/giphy.gif?cid=ecf05e47w0k5440n911hxowkn6lmi98zh7ouadmdgcujjvzk&ep=v1_gifs_related&rid=giphy.gif&ct=g"
-              />
-            </Card> */}
 
           <p className='font-mono text-sm '>
             I create educational content for developers, teaching them about web development, JavaScript and TypeScript, React and Next.js, and more. This comes in all forms: blog posts, videos, tweets, conference talks, and workshops. You can watch some of my favorites below.
@@ -142,7 +107,6 @@ export default function Home() {
           <h3 className='font-mono font-bold text-lg my-3 border-b-[1px] border-gray-800 py-3'>My work</h3>
 
           <p className='font-mono text-sm  my-5'>
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
             On a mission to build products developers love, and along the way, teach the next generation of developers. Here's a summary of my work so far.
           </p>
 
@@ -177,8 +141,6 @@ export default function Home() {
               </div>
             ))}
           </div>
-
-
         </div>
       </main>
     </NextUIProvider >
